d06bf9bc7eceeeb390f331f98615e6971f7046c9
kdvtrifork <kdv@trifork.com>
2020-10-21 13:39:47 +0200

    Add extension for urlsession

---
+++ Sources/TriforkSwiftNetworking/URLSession+NetworkSession.swift
@@ -0,0 +1,10 @@
+//
+//  URLSession+NetworkSession.swift
+//
+//  Created by Kim de Vos on 21/10/2020.
+//  Copyright © 2020 Trifork Public A/S. All rights reserved.
+//
+
+import Foundation
+
+extension URLSession: NetworkSession { }
