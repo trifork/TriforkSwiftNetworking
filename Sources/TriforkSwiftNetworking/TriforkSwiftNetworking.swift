4b65ea793c9e24359958691e59bab884b13bde31
kdvtrifork <kdv@trifork.com>
2020-10-21 13:33:42 +0200

    Remove boiler plate code

--- Sources/TriforkSwiftNetworking/TriforkSwiftNetworking.swift
+++
@@ -1,3 +0,0 @@
-struct TriforkSwiftNetworking {
-    var text = "Hello, World!"
-}
--- Tests/TriforkSwiftNetworkingTests/TriforkSwiftNetworkingTests.swift
+++ Tests/TriforkSwiftNetworkingTests/TriforkSwiftNetworkingTests.swift
@@ -6,7 +6,7 @@ final class TriforkSwiftNetworkingTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TriforkSwiftNetworking().text, "Hello, World!")
+        // XCTAssertEqual(TriforkSwiftNetworking().text, "Hello, World!")
     }
 
     static var allTests = [
