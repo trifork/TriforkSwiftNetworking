4524c8a73397d4f60c7540810a14aac4854c8f4d
Thomas Kalhøj Clemensen <tkc@trifork.com>
2020-11-30 11:16:23 +0100

    Availability

--- Sources/TriforkSwiftNetworking/Config/Logger.swift
+++ Sources/TriforkSwiftNetworking/Config/Logger.swift
@@ -3,7 +3,7 @@ import os.log
 
 final class Logger {
     static func log(_ message: String) {
-        if #available(iOS 10, *) {
+        if #available(iOS 10.0, watchOS 3.0, macOS 10.12, tvOS 10.0, *) {
             os_log("%@", log: OSLog(subsystem: "TriforkSwiftNetworking", category: "Networking"), type: .debug, message)
         } else {
             print("%@", message)
