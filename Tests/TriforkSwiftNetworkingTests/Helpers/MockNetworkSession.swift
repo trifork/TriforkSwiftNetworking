a0eae0ab632f92473385fdf08bc05042029ffd7a
Kim de Vos <kdv@trifork.com>
2021-03-01 18:25:39 +0100

    Add data task with completion

--- Tests/TriforkSwiftNetworkingTests/Helpers/MockNetworkSession.swift
+++ Tests/TriforkSwiftNetworkingTests/Helpers/MockNetworkSession.swift
@@ -11,6 +11,12 @@ final class MockNetworkSession: NetworkSession {
         return URLSession.shared.dataTask(with: request)
     }
 
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        self.request = request
+
+        return URLSession.shared.dataTask(with: request)
+    }
+
     @available(iOS 13.0, *)
     func dataTaskPublisher(for request: URLRequest) -> URLSession.DataTaskPublisher {
         self.request = request
