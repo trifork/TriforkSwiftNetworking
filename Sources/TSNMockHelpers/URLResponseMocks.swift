23ba079a8025214fb36daf1cbce0a892e3fdedf6
Thomas Kalhøj Clemensen <tkc@trifork.com>
2021-03-10 15:47:16 +0100

    Changed order of parameters

--- Sources/TSNMockHelpers/URLResponseMocks.swift
+++ Sources/TSNMockHelpers/URLResponseMocks.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class URLResponseMocks {
-    public static func response(statusCode: Int = 200, url: URL, headers: [String: String]? = nil) -> HTTPURLResponse? {
+    public static func response(url: URL, statusCode: Int = 200, headers: [String: String]? = nil) -> HTTPURLResponse? {
         HTTPURLResponse(
             url: url,
             statusCode: statusCode,
